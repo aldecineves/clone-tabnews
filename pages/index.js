8cff3fe77195af158319387ab6dfb1f28d7d0d1b
Aldeci Neves <aldecineves03@gmail.com>
2025-11-16 13:00:28 +0000

    adiciona scritps `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
-function Home(){
-    return <h1>Parabéns Felipe, o curso tá massa! Parabéns turma! Vamos em frente!!🎉</h1>
+function Home() {
+  return (
+    <h1>
+      {" "}
+      Parabéns Felipe, o curso tá massa! Parabéns turma! Vamos em frente!!🎉
+    </h1>
+  );
 }
+
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("identação errada");
+}
+
 export default Home;

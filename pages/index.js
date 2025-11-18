74bdc7252b364ca574cacc85608d7fa12f7de739
Aldeci Neves <aldecineves03@gmail.com>
2025-11-18 08:10:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      {" "}
-      Parabéns Felipe, o curso tá massa! Parabéns turma! Vamos em frente!!🎉
-    </h1>
-  );
-}
-
-function teste() {
-  console.log("teste");
-}
-
-function teste() {
-  console.log("identação errada");
+  return <h1> Parabéns Felipe, o curso tá massa! Parabéns turma! Vamos em frente!!🎉</h1>;
 }
 
 export default Home;
